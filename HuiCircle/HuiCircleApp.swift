7b93c90efe7b07c1f7f0404068ab57b9116d7778
Khanh Nguyen <duykhanhlb4999@gmail.com>
2026-03-28 12:27:54 +0700

    Initial Commit

---
+++ HuiCircle/HuiCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuiCircleApp.swift
+//  HuiCircle
+//
+//  Created by KhanhNguyen on 28/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HuiCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
